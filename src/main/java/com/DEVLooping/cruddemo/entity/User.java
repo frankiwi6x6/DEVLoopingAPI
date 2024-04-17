f2c1524262821ebe32a8c3f6b943bdd7969c9b6a
frankiwi6x6 <fra.reyesj@duocuc.cl>
2024-04-17 00:38:07 -0400

    Updated DB & userService

--- src/main/java/com/DEVLooping/cruddemo/entity/User.java
+++ src/main/java/com/DEVLooping/cruddemo/entity/User.java
@@ -1,5 +1,7 @@
 package com.DEVLooping.cruddemo.entity;
 
+import java.sql.Date;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -9,38 +11,28 @@ public class User {
     // definir campos
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "id_user")
     private int id;
-    @Column(name = "email")
-    private String email;
-    @Column(name = "bio")
-    private String bio;
-    @Column(name = "country_id")
-    private int country_id;
-    @Column(name = "profile_pic")
-    private String profile_pic;
     @Column(name = "username")
     private String username;
-    @Column(name = "dob")
-    private String dob;
-    @Column(name = "password")
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password_hashed")
     private String password;
+    @Column(name = "created_at")
+    private Date created_at;
 
     // definir constructores
+
     public User() {
     }
 
-    public User(String email, String bio, int country_id, String profile_pic, String username, String dob,
-            String password) {
+    public User(String email, String username, String password, Date created_at) {
         this.email = email;
-        this.bio = bio;
-        this.country_id = country_id;
-        this.profile_pic = profile_pic;
         this.username = username;
-        this.dob = dob;
         this.password = password;
+        this.created_at = created_at;
     }
-    // definir getters y setters
 
     public int getId() {
         return id;
@@ -58,44 +50,12 @@ public class User {
         this.email = email;
     }
 
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    public int getCountry_id() {
-        return country_id;
-    }
-
-    public void setCountry_id(int country_id) {
-        this.country_id = country_id;
-    }
-
-    public String getProfile_pic() {
-        return profile_pic;
-    }
-
-    public void setProfile_pic(String profile_pic) {
-        this.profile_pic = profile_pic;
-    }
-
     public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
     }
 
     public String getPassword() {
@@ -106,11 +66,25 @@ public class User {
         this.password = password;
     }
 
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    // definir getters y setters
+
     @Override
     public String toString() {
-        return "User [id=" + id + ", email=" + email + ", bio=" + bio + ", country_id=" + country_id
-                + ", profile_pic=" + profile_pic + ", username=" + username + ", dob=" + dob + ", password=" + password
-                + "]";
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", created_at='" + created_at + '\'' +
+                '}';
     }
 
 }
