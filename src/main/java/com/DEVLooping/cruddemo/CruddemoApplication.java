08514ed81b488cefabde769c729593026e65c906
frankiwi6x6 <fra.reyesj@duocuc.cl>
2024-04-18 15:59:30 -0400

    updated API information

--- src/main/java/com/DEVLooping/cruddemo/CruddemoApplication.java
+++ src/main/java/com/DEVLooping/cruddemo/CruddemoApplication.java
@@ -2,6 +2,9 @@ package com.DEVLooping.cruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -9,5 +12,11 @@ public class CruddemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
-
+	@Bean
+	public OpenAPI customOpenAPI(){
+		return new OpenAPI().info(new io.swagger.v3.oas.models.info.Info()
+		.title("Devlooping | API de Usuarios")
+		.version("1.0")
+		.description("Documentación para la API de gestión de usuarios de Devlooping"));
+	}
 }
